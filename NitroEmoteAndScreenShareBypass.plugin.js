5132876308d214afed1f0f56f8f17e96ce027370
PlagueCreator <92044981+PlagueCreator@users.noreply.github.com>
2021-11-07 18:43:19 +0100

    Update NitroEmoteAndScreenShareBypass.plugin.js

--- NitroEmoteAndScreenShareBypass.plugin.js
+++ NitroEmoteAndScreenShareBypass.plugin.js
@@ -7,54 +7,72 @@ class NitroEmoteAndScreenShareBypass {
         return "Send nitro emotes (lame bypass) and enable high quality screen sharing without Nitro";
     }
     getVersion() {
-        return "3.2";
+        return "3.5";
     }
     getAuthor() {
         return "oSumAtrIX";
     }
-    stop() {}
-	
+    stop() {
+
+    }
     start() {
-        const checkExist = setInterval(() => {	
-    		for (const mdl of Object.values(webpackJsonp.push([[],{['']:(_,e,r)=>{e.cache=r.c}},[['']]]).cache)) {
-    		    const d = mdl.exports.default;
-    		    if (d && d['getCurrentUser']) d.getCurrentUser().premiumType = 2;
-    		}
-        	clearInterval(checkExist);
-    	}, 1000);
+        const mod = BdApi.findModuleByProps("getCurrentUser")
+        let tries;
+        const checkExist = setInterval(() => {
+            let cUser;
+            if ((cUser = mod.getCurrentUser()) != undefined) {
+                cUser.premiumType = 2;
+                tries = 10;
+            }
+            if (++tries > 10)
+                clearInterval(checkExist);
+        }, 1000);
     }
 
     onSwitch() {
-        const useFileUpload = false;
-        const div = document.getElementsByClassName("name-3YKhmS")[0];
-	    const serverName = div != undefined ? div.innerHTML : "noServer";
-        const btn = $(".buttonContainer-28fw2U")[1];
+        const
+            useFileUpload = false,
+            div = document.getElementsByClassName("name-3YKhmS")[0],
+            serverName = div != undefined ? div.innerHTML : "noServer",
+            btnContainer = document.getElementsByClassName("buttons-3JBrkn")[0].children,
+            btn = btnContainer[btnContainer.length - 1];
+
         if (btn != null)
             btn.onclick = () => {
+
                 const checkExist = setInterval(function() {
-                    const scroller = $(".listItems-1uJgMC")[0];
+                    const scroller = document.getElementsByClassName("listItems-1uJgMC")[0];
                     if (scroller == null) return;
                     clearInterval(checkExist);
                     scroller.parentElement.onclick = (e) => {
-                        const target = e.target;
-                        const src = target.firstChild.src;
-			const ext = src.slice(-7, -4);
-			    
-                        if (ext == "gif" || target.parentElement.parentElement.children[0].firstChild.children[1].innerHTML != serverName) {
+                        const
+                            target = e.target,
+                            src = target.firstChild.src;
+                        if (src.slice(-7, -4) == "gif" || target.parentElement.parentElement.children[0].firstChild.children[1].innerHTML != serverName) {
                             const curChannel = BdApi.findModuleByProps("getLastSelectedChannelId").getChannelId();
-                            const url = src.slice(0, -4) + "?size=40";
+                            var url = src.slice(0, -4);
+                            var ext = url.slice(url.length - 3);
+
+                            function upLoad(blob) {
+                                BdApi.findModuleByProps("instantBatchUpload").instantBatchUpload(curChannel, [new File([blob], 'oSumAtrIX.' + ext, blob)]);
+                            }
+
+                            var txtBar = document.getElementsByClassName("textArea-12jD-V")[0];
+                            url = url + "?size=40";
                             useFileUpload ?
                                 fetch(url)
-                                .then(res => res.blob()).then(blob => {
-                                    BdApi.findModuleByProps("instantBatchUpload").instantBatchUpload(curChannel, [new File([blob], 'oSumAtrIX.'+ext, blob)]);
-                                }) :
+                                .then(res => res.blob()).then(upLoad) :
                                 BdApi.findModuleByProps("sendMessage").sendMessage(curChannel, {
                                     content: url
                                 });
+
+
+
                         }
+
                     }
                 }, 100);
+
             }
     }
 }
-
